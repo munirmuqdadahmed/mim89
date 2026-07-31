3953289415bce9de22cf4f31f35a26cbad2428d2
munirmuqdadahmed <monm51114@gmail.com>
2026-07-31 05:16:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Welcome To MIM89");
+document.querySelectorAll("nav a").forEach(link=>{
+
+link.addEventListener("click",function(e){
+
+const id=this.getAttribute("href");
+
+if(id.startsWith("#")){
+
+e.preventDefault();
+
+document.querySelector(id).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
+
+});
+
+});
+
+console.log("MIM89 Ready");
