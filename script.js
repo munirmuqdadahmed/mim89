9bd9e42a440094a286298b1661c52d2bbf7df152
munirmuqdadahmed <monm51114@gmail.com>
2026-07-31 05:37:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,28 @@ current=0;
 slides[current].classList.add("active");
 
 },4000);
+const search=document.getElementById("search");
+
+search.addEventListener("keyup",function(){
+
+const value=this.value.toLowerCase();
+
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+const text=card.innerText.toLowerCase();
+
+if(text.includes(value)){
+
+card.style.display="inline-block";
+
+}else{
+
+card.style.display="none";
+
+}
+
+});
+
+});
