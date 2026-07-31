28ea052f29b7b65be158dbbbaf1a1c5d503e9402
munirmuqdadahmed <monm51114@gmail.com>
2026-07-31 05:29:40 +0300

    Add automatic slide transition functionality
    Implement automatic slide transition every 4 seconds.

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ document.getElementById("loader").style.display="none";
 },1500);
 
 }
+const slides=document.querySelectorAll(".slide");
+
+let current=0;
+
+setInterval(()=>{
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current>=slides.length){
+
+current=0;
+
+}
+
+slides[current].classList.add("active");
+
+},4000);
