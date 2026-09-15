acc95033da2aa6578bb5641d00471568dc1bb6b1
munirmuqdadahmed <monm51114@gmail.com>
2026-09-15 16:41:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.addEventListener('keydown', event => {
 });
 
 const MIM89_VERSION     = "1100";
-const MIM89_APP_VERSION = '1752';
+const MIM89_APP_VERSION = '1753';
 
 /* ==========================================
    المتغيرات العامة
